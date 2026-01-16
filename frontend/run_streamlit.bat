d18b958811577ad01adc53691d22ac5e4bafe7ec
Aayush Walsangikar <aayushwalsangikar118@gmail.com>
2026-01-16 18:07:37 +0530

    Delete frontend directory

--- frontend/run_streamlit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Helper to run Streamlit using the same Python interpreter (avoids PATH issues)
-if "%1"=="" (
-  python -m streamlit run "%~dp0app.py"
-) else (
-  python -m streamlit run "%~dp0%1"
-)
